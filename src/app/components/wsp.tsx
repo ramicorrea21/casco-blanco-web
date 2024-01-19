4067b3dc4b6dba8b1565a3581803f36d9fb5e2f4
ramicorrea21 <ramicorrea021@gmail.com>
2024-01-19 11:41:34 -0300

    fixed button

--- src/app/components/wsp.tsx
+++ src/app/components/wsp.tsx
@@ -1,36 +1,35 @@
 import Image from "next/image";
 import Link from "next/link";
-export default function WspButton() {
-  const phoneNumber = "+54 9 3516 95-7869";
-  const message = "Hola! queria hacer una consulta sobre Casco Blanco!."; 
 
-  const encodedMessage = encodeURIComponent(message);
-  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
+export default function WspButton() {
+  const phoneNumber = "+5493516957869";
+  const message = "Hola! quería hacer una consulta sobre Casco Blanco.";
+
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${encodeURIComponent(phoneNumber)}&text=${encodeURI(message)}`;
 
   return (
     <>
       <div>
         <Link
-        href={whatsappLink}
-        target="_blank"
-        rel="noopener noreferrer"
+          href={whatsappLink}
+          target="_blank"
+          rel="noopener noreferrer"
         >
-        <Image
-          src="/img/whatsapp.png"
-          alt="whatsapp"
-          width={300}
-          height={150}
-          className="fixed hidden md:block bottom-4 right-4 rounded-full cursor-pointer"
-        />
-        <Image
-          src="/img/whatsapp.png"
-          alt="whatsapp"
-          width={200}
-          height={100}
-          className="fixed md:hidden bottom-4 right-4 rounded-full cursor-pointer"
-        />
+          <Image
+            src="/img/whatsapp.png"
+            alt="whatsapp"
+            width={300}
+            height={150}
+            className="fixed hidden md:block bottom-4 right-4 rounded-full cursor-pointer"
+          />
+          <Image
+            src="/img/whatsapp.png"
+            alt="whatsapp"
+            width={200}
+            height={100}
+            className="fixed md:hidden bottom-4 right-4 rounded-full cursor-pointer"
+          />
         </Link>
-        
       </div>
     </>
   );
